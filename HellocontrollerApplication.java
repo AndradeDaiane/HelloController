0517e83c82024cc1b0dce9e8dd2734675d57721e
Daiane dos Santos Andrade <161532533+AndradeDaiane@users.noreply.github.com>
2025-02-28 21:52:22 -0300

    Add files via upload

---
+++ HellocontrollerApplication.java
@@ -0,0 +1,24 @@
+package com.seuprojeto.controller.hellocontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/test")
+public class HellocontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellocontrollerApplication.class, args);
+	}
+
+	@GetMapping("/test")
+	public String helloWorld() {
+		return "Hello, World!";
+	}
+	
+}
